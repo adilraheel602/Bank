5ad3328718c382b0cbda36d6cf884c6bcc0d8d43
M. Adil <76224599+adilraheel602@users.noreply.github.com>
2023-05-04 16:39:15 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,129 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import figlet from "figlet";
+import chalk from "chalk";
+
+class Customer{
+    fName: string;
+    lName: string;
+    gender: string;
+    age: number;
+    contact: string;
+    accountBalance:number = 25000;
+    constructor(fName:string,lName:string,gender:string, age:number, contact:string){
+        this.fName = fName;
+        this.lName = lName;
+        this.gender = gender;
+        this.age = age;
+        this.contact = contact;
+    } 
+}
+
+interface bankAccountInterface{
+    debit:()=>{};
+    credit:() =>{};
+}
+
+class BankAccount   {
+    debit=(debitAmount: number,customer:Customer)=>{    //if debit amount is less than balance then it will cancel transaction with a message.
+        if(debitAmount<=customer.accountBalance){
+            customer.accountBalance -= debitAmount;
+            console.log(`Your ${debitAmount} is debited from your account`);
+        }
+        else{
+            console.log("Your account don't have enough balance.");
+        }
+    } ;    
+    credit = (creditAmount:number, customer:Customer)=>{ //1$ will be deducted if more than 100$ is credited.
+        if(creditAmount>=100){
+            creditAmount -=1;
+            customer.accountBalance+=creditAmount;
+            console.log(`Your ${creditAmount} is credited to your account`);
+        }
+        else{
+            customer.accountBalance+=creditAmount;
+            console.log(`Your ${creditAmount} is credited from your account`);
+        }
+    };  
+    checkBalance = (customer:Customer)=>{
+        console.log(customer.accountBalance);
+    };
+}
+
+const customer1 = new Customer("Adil", "Raheel","Male", 26, "2141215");
+console.log(customer1);
+
+let BankAccount1 = new BankAccount();
+console.log(BankAccount1);
+
+
+let select = async() => {
+    const input = await inquirer.prompt([
+        {
+            type:"number",
+            name: "selection",
+            message: "Enter 1 for next task otherwise enter 2: ",
+        }
+    ]);
+    if(input.selection==1){
+        await welcome()
+    }
+    else{
+        console.log(chalk.bgMagentaBright("GoodBye! See you next time."));
+    }
+}
+
+let welcome =async () => {
+    console.log(figlet.textSync('Welcome to MyBank!'));
+    console.log("Please select the operation you want to perform:");
+    const input = await inquirer.prompt([{
+        type: "list",
+        name: "Operation",
+        choices: ["Debit","Credit", "Balance Check"],
+    }
+    ]);
+    console.log(`You wants to perform ${input.Operation}`);
+    if(input.Operation == "Debit"){
+        const input = await inquirer.prompt([{
+            type: "number",
+            name: "debitAmount",
+            message: "Enter amount you want to debit! ",
+        }
+    ]);
+    console.log("entered");
+    BankAccount1.debit(input.debitAmount,customer1)
+    }
+    else if(input.Operation=="Credit"){
+        const input = await inquirer.prompt([{
+            type: "number",
+            name: "creditAmount",
+            message: "Enter amount you want to credit! ",
+        }
+    ]);
+    BankAccount1.credit(input.creditAmount,customer1);
+    }
+    else if(input.Operation=="Balance Check"){
+        BankAccount1.checkBalance(customer1)
+    }
+    else{
+        console.log("You didn't choose anything.");
+    }
+    await select()
+}
+
+await welcome()
+
+
+
+// const welcome = async() =>{
+//     const input = await inquirer.prompt([
+//         {
+//             type: "number",
+//             name: "num1",
+//             message: "Enter a number: ",
+//         }
+// ]);
+// console.log(input.num1);
+
+// }
+// welcome()
